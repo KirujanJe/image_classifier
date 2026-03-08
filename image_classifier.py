e029e63ff8eadfa92303d4e5c15b46760bf21e3a
Kirujan <jegathki@sheridancollege.ca>
2026-03-08 01:39:22 -0500

    Delete image_classifier.py

--- image_classifier.py
+++
@@ -1,7 +0,0 @@
-# Image classification project
-# Tech Stack: Python, UV, Streamlit, Tensorflow, numpy, OpenCV
-
-# notes
-# to initialize uv: uv init
-# to add dependencies: uv add streamlit tensorflow opencv-python
-# then go to main.py after all are downloaded
